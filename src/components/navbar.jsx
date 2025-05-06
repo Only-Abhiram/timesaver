04bec29ef507eb448d14b216027df2f6e9157022
Only-Abhiram <abhi1@AbhiChikki.(none)>
2025-05-06 13:24:24 +0530

    update2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import Dropdown from "./dropdown"
 import {tools} from './toolscollection.jsx';
 import Loginup from './Loginup';
 export default function Navbar() {
-    const [popup, setPopup] = useState(false)
+    
     return <header className="backdrop-blur-[5px] sticky top-0 z-50 bg-white/30 border-b border-gray-100 px-4 md:px-6 py-5">
         <div className="max-w-7xl mx-auto flex items-center justify-between">
             <div className="flex items-center space-x-6 ">
@@ -57,13 +57,11 @@ export default function Navbar() {
                 </div>
 
                 <button onClick={()=>{
-                    setPopup(true);
+                    
                 }} className="bg-[#5D5FEF] hover:text-[#5D5FEF] hover:bg-white border-2 border-[#5D5FEF] text-white rounded-full px-4 py-2 text-sm font-medium flex items-center h-8 transition-all ease-in-out cursor-pointer">
                     <span className="">SignIn</span>
                     {
-                        popup && (
-                            <Loginup/>
-                        )
+                        
                     }
                 </button>
             </div>
